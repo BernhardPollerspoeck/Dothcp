8648149b80a0ad06e247b4c9d5b23a46a9bfb0ea
Bernhard Pollerspöck <bernhard@pollerspoeck.net>
2025-05-27 21:03:36 +0200

    fix minor

--- src/qt.qsp.dhcp.Server/Grains/DhcpManager/DhcpLease.cs
+++ src/qt.qsp.dhcp.Server/Grains/DhcpManager/DhcpLease.cs
@@ -2,26 +2,37 @@
 
 namespace qt.qsp.dhcp.Server.Grains.DhcpManager;
 
+[GenerateSerializer]
 public class DhcpLease
 {
 	// Client identification
-	public string MacAddress { get; set; } = string.Empty;
-	public IPAddress IpAddress { get; set; } = IPAddress.None;
-	public string? HostName { get; set; }
+	  [Id(0)]
+    public string MacAddress { get; set; } = string.Empty;
+    [Id(1)]
+    public IPAddress IpAddress { get; set; } = IPAddress.None;
+    [Id(2)]
+    public string? HostName { get; set; }
 
-	// Lease timing
-	public TimeSpan LeaseDuration { get; set; } = TimeSpan.FromDays(1);
-	public DateTime LeaseStart { get; set; } = DateTime.UtcNow;
+    // Lease timing
+    [Id(3)]
+    public TimeSpan LeaseDuration { get; set; } = TimeSpan.FromDays(1);
+    [Id(4)]
+    public DateTime LeaseStart { get; set; } = DateTime.UtcNow;
 	public DateTime LeaseExpiration => LeaseStart.Add(LeaseDuration);
 	
 	// Lease status
-	public LeaseStatus Status { get; set; } = LeaseStatus.Active;
+	  [Id(5)]
+    public LeaseStatus Status { get; set; } = LeaseStatus.Active;
 
-	// Network configuration
-	public IPAddress? Subnet { get; set; }
-	public IPAddress? Router { get; set; }
-	public IPAddress? DhcpServer { get; set; }
-	public IList<IPAddress> DnsServers { get; set; } = [];
+    // Network configuration
+    [Id(6)]
+    public IPAddress? Subnet { get; set; }
+    [Id(7)]
+    public IPAddress? Router { get; set; }
+    [Id(8)]
+    public IPAddress? DhcpServer { get; set; }
+    [Id(9)]
+    public IList<IPAddress> DnsServers { get; set; } = [];
 
 	// Methods for lease management
 	public bool IsExpired() => DateTime.UtcNow > LeaseExpiration;
